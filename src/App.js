c21554922919d734ade7cae66d06a889f7d2344e
soerson <soerson@pm.me>
2020-08-09 21:15:27 +0300

    Get signature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React      from 'react';
+import { config } from "./config"
 import './App.css';
 
 function App() {
+  const {
+    signatureEndpoint,
+    meetingNumber,
+    role,
+  } = config;
+
+  const getSignature = () => {
+    fetch(signatureEndpoint, {
+      method: 'POST',
+      body: JSON.stringify({
+        meetingNumber,
+        role
+      })
+    })
+      .then(res => res.json())
+      .then(data => console.log(data.signature))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getSignature}>Join meeting</button>
     </div>
   );
 }
---
+++ src/config.sample.js
@@ -0,0 +1,10 @@
+export const config = {
+  signatureEndpoint: '',
+  apiKey: '',
+  meetingNumber: 1234567890,
+  role: 0,
+  leaveUrl: 'http://localhost:3000',
+  userName: 'Milo',
+  userEmail: '',
+  passWord: '',
+}
