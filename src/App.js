606e3d43b6a4d663ad5e31c3a26089749d2c6ef6
soerson <soerson@pm.me>
2020-08-10 11:10:47 +0300

    Join the meeting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,58 @@
-import React      from 'react';
-import { config } from "./config"
+import React, { useEffect } from 'react';
+import { config }           from './config'
+import { ZoomMtg } from '@zoomus/websdk'
+
 import './App.css';
+
 
 function App() {
   const {
     signatureEndpoint,
     meetingNumber,
     role,
+    leaveUrl,
+    userName,
+    apiKey,
+    userEmail,
+    passWord
   } = config;
+
+  useEffect(() => {
+    ZoomMtg.setZoomJSLib("https://source.zoom.us/1.7.10/lib", "/av");
+    ZoomMtg.preLoadWasm();
+    ZoomMtg.prepareJssdk();
+  }, [])
+
+  const joinMeeting = (signature) => {
+    document.getElementById('zmmtg-root').style.display = 'block'
+
+    ZoomMtg.init({
+      leaveUrl,
+      isSupportAV: true,
+      success: (success) => {
+        console.log('Init success: ', success)
+
+        ZoomMtg.join({
+          signature,
+          meetingNumber,
+          userName,
+          apiKey,
+          userEmail,
+          passWord,
+          success: (success) => {
+            console.log('Join success: ', success)
+          },
+          error: (error) => {
+            console.error('Join error: ', error)
+          }
+        })
+
+      },
+      error: (error) => {
+        console.log(error)
+      }
+    })
+  }
 
   const getSignature = () => {
     fetch(signatureEndpoint, {
@@ -18,7 +63,14 @@ function App() {
       })
     })
       .then(res => res.json())
-      .then(data => console.log(data.signature))
+      .then(data => {
+        if (data.signature) {
+          console.log(data.signature)
+          joinMeeting(data.signature)
+        } else {
+          console.error('Could not get a signature.', data)
+        }
+      })
   }
 
   return (
